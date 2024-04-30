4ace37fd55fee8bdc1f484d870bcc5593287364a
kainholy <ookuraseiya0506@gmail.com>
2024-04-30 20:20:17 +0900

    修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,6 @@ import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import { client } from "../../libs/client";
 import { useEffect, useState } from "react";
 import Loading from "@/components/sections/top/Loading";
-import { useRouter } from "next/router";
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
@@ -36,26 +35,14 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 const Home: NextPage<Props> = ({ news }) => {
-  const router = useRouter();
   
   const [isLoading, setIsLoading] = useState(true);
-
   useEffect(() => {
-    const handleStart = (url: string) => {
-      url !== router.pathname ? setIsLoading(true) : setIsLoading(false);
-    };
-    const handleComplete = () => setIsLoading(false);
-
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleComplete);
-    router.events.on('routeChangeError', handleComplete);
-
-    return () => {
-      router.events.off('routeChangeStart', handleStart);
-      router.events.off('routeChangeComplete', handleComplete);
-      router.events.off('routeChangeError', handleComplete);
-    };
-  }, [router]);
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 10000);
+  }, [isLoading])
   return (
     <>
       <Head>
