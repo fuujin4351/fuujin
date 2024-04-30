258fa4fb98c6bdec12f10fa62c454d40972712c5
wadada <w.daiki1175@gmail.com>
2024-04-30 19:31:07 +0900

    next/scriptに変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
-// 例えば、ページ間で共有されるナビゲーションバーやフッター、グローバルCSS、または状態管理ロジックなどをここに配置することができる
-
 import "@/styles/globals.scss";
 import "@/styles/normalize.scss";
 import "@/styles/mixin.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script"; // next/script をインポートします
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -15,31 +14,24 @@ export default function App({ Component, pageProps }: AppProps) {
           href="/favicon/favicon.svg"
           sizes="any"
           type="image/svg+xml"
-        ></link>
-        <link
-          rel="apple-touch-icon"
-          href="/favicon/apple-touch-icon.png"
-        ></link>
-        <link rel="icon" href="/favicon/favicon.png"></link>
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta property="og:image" content="/ogp.png"/>
-        <meta property="og:image:secure_url" content="/ogp.png"/>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+        />
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon/favicon.png" />
+      </Head>
+      <Script strategy="lazyOnload">
+        {" "}
+        {/* スクリプトを遅延読み込みで設定 */}
+        {`
           (function(d) {
             var config = {
               kitId: 'cro5gvm',
               scriptTimeout: 3000,
               async: true
             },
-            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
           })(document);
-        `,
-          }}
-        />
-      </Head>
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
