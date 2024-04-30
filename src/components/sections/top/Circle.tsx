88e9c7aa1e80dac7688350d224c026493cd55bac
wadada <w.daiki1175@gmail.com>
2024-04-30 18:03:39 +0900

    fixed

--- src/components/sections/top/Circle.tsx
+++ src/components/sections/top/Circle.tsx
@@ -1,40 +1,51 @@
-import React, { useEffect, useRef } from 'react'
-import styles from '@/styles/sections/top/Circle.module.scss'
-import Image from 'next/image'
+import React, { useEffect, useRef } from "react";
+import styles from "@/styles/sections/top/Circle.module.scss";
+import Image from "next/image";
 import gsap from "gsap";
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
-gsap.registerPlugin(ScrollTrigger)
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 function Circle() {
-    const containerRef = useRef(null)
-    const stickyRef = useRef(null)
-    const circleImgRef = useRef(null)
-    
-    useEffect(() => {
-        const sticky = stickyRef.current;
-        const container = containerRef.current;
-        const containerHeight = container.clientHeight;
-        const circleImg = circleImgRef.current;
+  const containerRef = useRef<HTMLDivElement>(null);
+  const stickyRef = useRef<HTMLDivElement>(null);
+  const circleImgRef = useRef<HTMLImageElement>(null);
 
-        gsap.fromTo(circleImg, {clipPath: 'circle(25%)'}, {
-            clipPath: 'circle(100%)',
-            scrollTrigger: {
-                trigger: sticky,
-                start: '10% top',
-                end: `+=${containerHeight}`,
-                scrub: true,
-            }
-        })
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return; // nullチェックを先に移動
+    const containerHeight = container.clientHeight;
+    const circleImg = circleImgRef.current;
+    const sticky = stickyRef.current;
 
-    })
+    gsap.fromTo(
+      circleImg,
+      { clipPath: "circle(25%)" },
+      {
+        clipPath: "circle(100%)",
+        scrollTrigger: {
+          trigger: sticky,
+          start: "10% top",
+          end: `+=${containerHeight}`,
+          scrub: true,
+        },
+      }
+    );
+  });
 
   return (
     <div className={styles.container} ref={containerRef}>
-        <div className={styles.sticky} ref={stickyRef}>
-            <Image src='/top/circle/1.png' ref={circleImgRef} className={styles.imgArea} alt='' width={1440} height={900}></Image>
-        </div>
+      <div className={styles.sticky} ref={stickyRef}>
+        <Image
+          src="/top/circle/1.png"
+          ref={circleImgRef}
+          className={styles.imgArea}
+          alt=""
+          width={1440}
+          height={900}
+        ></Image>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Circle
+export default Circle;
--- src/components/sections/top/Mv.tsx
+++ src/components/sections/top/Mv.tsx
@@ -1,14 +1,19 @@
-import React, { useEffect, useRef } from 'react'
-import Image from 'next/image'
-import styles from '@/styles/sections/top/Mv.module.scss'
-import SwiperCore, { Autoplay, Navigation, Pagination, EffectFade } from "swiper/modules";
+import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import styles from "@/styles/sections/top/Mv.module.scss";
+import SwiperCore, {
+  Autoplay,
+  Navigation,
+  Pagination,
+  EffectFade,
+} from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import gsap from "gsap";
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
-gsap.registerPlugin(ScrollTrigger)
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const images = [
   "/top/mv/1.webp",
@@ -21,46 +26,44 @@ const images = [
 ];
 
 function Mv() {
-  const titleRef = useRef(null)
-  const containerRef = useRef(null)
+  const titleRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const title = titleRef.current
-    const container = containerRef.current
-    const titleHeight = title.clientHeight
-    const windowHeight = window.innerHeight
-    const initialTop = windowHeight - titleHeight - 40
+    const title = titleRef.current;
+    const container = containerRef.current;
+    if (!title || !container) return;
+    const titleHeight = title.clientHeight;
+    const windowHeight = window.innerHeight;
+    const initialTop = windowHeight - titleHeight - 40;
 
-    const mm = gsap.matchMedia()
-    
-    mm.add('(min-width: 769px)', () => {
-      gsap.set(title, {top: initialTop})
-  
+    const mm = gsap.matchMedia();
+
+    mm.add("(min-width: 769px)", () => {
+      gsap.set(title, { top: initialTop });
+
       gsap.to(title, {
         scrollTrigger: {
           trigger: container,
-          start: 'top bottom',
-          end: 'bottom bottom',
+          start: "top bottom",
+          end: "bottom bottom",
           scrub: 0,
           onUpdate: (self) => {
-            const top = initialTop + self.scroll.v
-            if (self.scroll.v >= 253) {
-              gsap.set(title, {top: initialTop + 253})
+            const top = initialTop + self.scroll();
+            if (self.scroll() >= 253) {
+              gsap.set(title, { top: initialTop + 253 });
             } else {
-              gsap.set(title, {top: top})
+              gsap.set(title, { top: top });
             }
-          }
-        }
-      })
-    })
-  })
-
+          },
+        },
+      });
+    });
+  });
 
   return (
-    <section className={styles.container} ref={containerRef} id='mv'>
-
-      <div className={styles.bg}>
-      </div>
+    <section className={styles.container} ref={containerRef} id="mv">
+      <div className={styles.bg}></div>
 
       <Swiper
         modules={[Navigation, Pagination, Autoplay, EffectFade]}
@@ -76,7 +79,7 @@ function Mv() {
         }} // スライド表示時間
         followFinger={false} // ユーザーの操作によるスライドの停止
         pagination={{
-          el: '.swiper-pagination',
+          el: ".swiper-pagination",
           clickable: false,
         }} // ページネーション, クリックで対象のスライドに切り替わる
         className={styles.slideWrapper}
@@ -88,20 +91,21 @@ function Mv() {
               width={1920}
               height={1038}
               alt="Slider Image"
-              className={styles.slideImage + ' slider-img'}
+              className={styles.slideImage + " slider-img"}
             />
           </SwiperSlide>
         ))}
       </Swiper>
 
       <div className={styles.titleArea} ref={titleRef}>
-        <h1 className={styles.title + ' c-mv-title --bk'}>
-          駆けろ<span className={styles.titleBlue + ' --blue'}>青</span>春、<br className='sp' />
+        <h1 className={styles.title + " c-mv-title --bk"}>
+          駆けろ<span className={styles.titleBlue + " --blue"}>青</span>春、
+          <br className="sp" />
           轟け躍動
         </h1>
       </div>
     </section>
-  )
+  );
 }
 
-export default Mv
+export default Mv;
