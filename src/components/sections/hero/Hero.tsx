bd9f646f00db9dbae0da74f0aeaad3bf70a336a7
wadada <w.daiki1175@gmail.com>
2024-06-20 01:18:51 +0900

    画像表示が遅いので(修正直ったかは不明)

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -2,26 +2,34 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "@/styles/hero/Hero.module.scss";
 
-const Hero: React.FC = () => {
-  const images = [
-    { src: "1.webp", styleClass: styles.img1 },
-    { src: "2.webp", styleClass: styles.img2 },
-    { src: "3.webp", styleClass: styles.img3 },
-    { src: "4.webp", styleClass: styles.img4 },
-    { src: "5.webp", styleClass: styles.img5 },
-    { src: "6.webp", styleClass: styles.img5 },
-    { src: "7.webp", styleClass: styles.img5 },
-    { src: "8.webp", styleClass: styles.img5 },
-    { src: "9.webp", styleClass: styles.img5 },
-    { src: "10.webp", styleClass: styles.img5 },
-    { src: "11.webp", styleClass: styles.img5 },
-    { src: "12.webp", styleClass: styles.img5 },
-    { src: "13.webp", styleClass: styles.img5 },
-  ];
+const images = [
+  { src: "1.webp", styleClass: styles.img1 },
+  { src: "2.webp", styleClass: styles.img2 },
+  { src: "3.webp", styleClass: styles.img3 },
+  { src: "4.webp", styleClass: styles.img4 },
+  { src: "5.webp", styleClass: styles.img5 },
+  { src: "6.webp", styleClass: styles.img5 },
+  { src: "7.webp", styleClass: styles.img5 },
+  { src: "8.webp", styleClass: styles.img5 },
+  { src: "9.webp", styleClass: styles.img5 },
+  { src: "10.webp", styleClass: styles.img5 },
+  { src: "11.webp", styleClass: styles.img5 },
+  { src: "12.webp", styleClass: styles.img5 },
+  { src: "13.webp", styleClass: styles.img5 },
+];
 
+const preloadImages = () => {
+  images.forEach((image) => {
+    const img = new window.Image();
+    img.src = `/hero/${image.src}`;
+  });
+};
+
+const Hero: React.FC = () => {
   const [currentImage, setCurrentImage] = useState({ src: "", styleClass: "" });
 
   useEffect(() => {
+    preloadImages();
     const randomIndex = Math.floor(Math.random() * images.length);
     setCurrentImage(images[randomIndex]);
   }, []);
@@ -34,7 +42,8 @@ const Hero: React.FC = () => {
             src={`/hero/${currentImage.src}`}
             alt="Random Image"
             className={currentImage.styleClass}
-            layout="fill"
+            width={1920} // 適切な幅を指定
+            height={400} // 適切な高さを指定
             objectFit="cover"
           />
         )}
