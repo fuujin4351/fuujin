259e0c6793bce325369c41b620e6a42362553ef1
wadada <w.daiki1175@gmail.com>
2024-07-21 14:33:39 +0900

    簡易修正

--- src/components/sections/about/Award.tsx
+++ src/components/sections/about/Award.tsx
@@ -1,53 +1,15 @@
+// Award.js
 import React, { useState, useEffect, useRef } from "react";
 import styles from "@/styles/sections/about/Award.module.scss";
+import { awardList } from "./awardList";
 
-interface Award {
-  year: string;
-  name: string;
-  prize: string;
+interface AwardItemProps {
+  award: {
+    year: string;
+    name: string;
+    prize: string;
+  };
 }
-
-const awardList: Award[] = [
-  { year: "2007年", name: "ちばYOSAKOI2007", prize: "ミニマム準大賞受賞" },
-  {
-    year: "2013年",
-    name: "ドリームよさこい2013",
-    prize: "ブロンズ賞(総合3位)受賞",
-  },
-  { year: "2014年", name: "YOSAKOI鎌ヶ谷", prize: "優秀賞受賞" },
-  { year: "2014年", name: "東京よさこい", prize: "豊島未来賞受賞" },
-  { year: "2015年", name: "ドリーム夜さ来い祭り", prize: "MC賞受賞" },
-  { year: "2016年", name: "大江戸ソーラン祭り", prize: "6位入賞" },
-  { year: "2016年", name: "東京よさこい", prize: "三菱地所株式会社賞受賞" },
-  {
-    year: "2016年",
-    name: "ドリーム夜さ来い祭り",
-    prize: "アクアシティお台場賞受賞",
-  },
-  { year: "2017年", name: "東京よさこい", prize: "那珂川町長賞受賞" },
-  {
-    year: "2018年",
-    name: "かみす舞っちゃげ祭り2018",
-    prize: "学生大会大賞 (初代大賞)",
-  },
-  {
-    year: "2018年",
-    name: "ドリーム夜さ来い祭り",
-    prize: "デックス東京ビーチ賞受賞",
-  },
-  { year: "2019年", name: "大江戸ソーラン祭り", prize: "5位入賞" },
-  {
-    year: "2019年",
-    name: "かみす舞っちゃげ祭り",
-    prize: "学生大会大賞(2連覇)",
-  },
-  {
-    year: "2019年",
-    name: "ドリーム夜さ来い祭り",
-    prize: "MC賞受賞(個人賞)",
-  },
-  { year: "2023年", name: "東京よさこい", prize: "那珂川町長賞受賞" },
-];
 
 const Award: React.FC = () => {
   const [text, setText] = useState<string>("");
@@ -110,10 +72,6 @@ const Award: React.FC = () => {
     </div>
   );
 };
-
-interface AwardItemProps {
-  award: Award;
-}
 
 const AwardItem: React.FC<AwardItemProps> = ({ award }) => (
   <div className={styles.award}>
---
+++ src/components/sections/about/awardList.ts
@@ -0,0 +1,47 @@
+interface Award {
+  year: string;
+  name: string;
+  prize: string;
+}
+
+export const awardList: Award[] = [
+  { year: "2007年", name: "ちばYOSAKOI2007", prize: "ミニマム準大賞受賞" },
+  {
+    year: "2013年",
+    name: "ドリームよさこい2013",
+    prize: "ブロンズ賞(総合3位)受賞",
+  },
+  { year: "2014年", name: "YOSAKOI鎌ヶ谷", prize: "優秀賞受賞" },
+  { year: "2014年", name: "東京よさこい", prize: "豊島未来賞受賞" },
+  { year: "2015年", name: "ドリーム夜さ来い祭り", prize: "MC賞受賞" },
+  { year: "2016年", name: "大江戸ソーラン祭り", prize: "6位入賞" },
+  { year: "2016年", name: "東京よさこい", prize: "三菱地所株式会社賞受賞" },
+  {
+    year: "2016年",
+    name: "ドリーム夜さ来い祭り",
+    prize: "アクアシティお台場賞受賞",
+  },
+  { year: "2017年", name: "東京よさこい", prize: "那珂川町長賞受賞" },
+  {
+    year: "2018年",
+    name: "かみす舞っちゃげ祭り2018",
+    prize: "学生大会大賞 (初代大賞)",
+  },
+  {
+    year: "2018年",
+    name: "ドリーム夜さ来い祭り",
+    prize: "デックス東京ビーチ賞受賞",
+  },
+  { year: "2019年", name: "大江戸ソーラン祭り", prize: "5位入賞" },
+  {
+    year: "2019年",
+    name: "かみす舞っちゃげ祭り",
+    prize: "学生大会大賞(2連覇)",
+  },
+  {
+    year: "2019年",
+    name: "ドリーム夜さ来い祭り",
+    prize: "MC賞受賞(個人賞)",
+  },
+  { year: "2023年", name: "東京よさこい", prize: "那珂川町長賞受賞" },
+];
