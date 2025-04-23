826e6fa982658fd6e3ac47392ffb4f67a4ba6d09
wadada <w.daiki1175@gmail.com>
2025-04-23 16:35:46 +0900

    next version up

--- src/components/elements/Twitter.tsx
+++ src/components/elements/Twitter.tsx
@@ -1,13 +1,28 @@
-"use client";
-import Script from "next/script";
+// "use client";
+// import { useEffect } from "react";
+// import Script from "next/script";
 
-export const Twitter = () => {
-    return (
-        <>
-            <a className="twitter-timeline" data-width="500" data-height="400" href="https://twitter.com/fujin_4351_tw?ref_src=twsrc%5Etfw">
-                Tweets by fujin_4351_tw
-            </a>
-            <Script async src="https://platform.twitter.com/widgets.js"></Script>
-        </>
-    );
-};
+// export const Twitter = () => {
+//   useEffect(() => {
+//     if ((window as any).twttr?.widgets) {
+//       (window as any).twttr.widgets.load();
+//     }
+//   }, []);
+
+//   return (
+//     <>
+//       <a
+//         className="twitter-timeline"
+//         data-width="500"
+//         data-height="600"
+//         href="https://twitter.com/fujin_4351_tw?ref_src=twsrc%5Etfw"
+//       >
+//         Tweets by fujin_4351_tw
+//       </a>
+//       <Script
+//         src="https://platform.twitter.com/widgets.js"
+//         strategy="afterInteractive"
+//       />
+//     </>
+//   );
+// };
--- src/components/sections/top/Sns.tsx
+++ src/components/sections/top/Sns.tsx
@@ -1,27 +1,44 @@
-import React from 'react'
-import styles from '@/styles/sections/top/Sns.module.scss'
-import { Twitter } from '@/components/elements/Twitter'
+import React from "react";
+import styles from "@/styles/sections/top/Sns.module.scss";
+import Image from "next/image";
 
 function Sns() {
   return (
-    <section className={styles.sns} id='sns'>
-        <div className={styles.container}>
-            <div className={styles.titleArea}>
-                <h2 className={styles.title + ' c-title2 --bk'}>公式SNS</h2>
-            </div>
-            <div className={styles.content}>
-                <div className={styles.snsArea}>
-                    <p className={styles.snsTitle + ' c-sns-title'}>Twitter</p>
-                    <Twitter />
-                </div>
-                <div className={styles.snsArea}>
-                    <p className={styles.snsTitle + ' c-sns-title'}>Youtube</p>
-                    <iframe width="560" height="400" src="https://www.youtube.com/embed/S8Ih0jG0a98?si=L4N5b5I7TNDZHS7D" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                </div>
-            </div>
+    <section className={styles.sns} id="sns">
+      <div className={styles.container}>
+        <div className={styles.titleArea}>
+          <h2 className={styles.title + " c-title2 --bk"}>
+            公式SNS・各種リンク
+          </h2>
         </div>
+        <div className={styles.content}>
+          <div className={styles.snsArea}>
+            <p className={styles.snsTitle + " c-sns-title"}>X(旧Twitter)</p>
+            <Image
+              src="/top/sns/logo-black.png"
+              alt="Twitter"
+              width={100}
+              height={100}
+              className={styles.snsIcon}
+            />
+          </div>
+          <div className={styles.snsArea}>
+            <p className={styles.snsTitle + " c-sns-title"}>Instagram</p>
+            <Image
+              src="/top/sns/instagram.svg"
+              alt="Instagram"
+              width={100}
+              height={100}
+              className={styles.snsIcon}
+            />
+          </div>
+          <div className={styles.snsArea}>
+            <p className={styles.snsTitle + " c-sns-title"}>Youtube</p>
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Sns
+export default Sns;
