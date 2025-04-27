89d53a7b10b94a8c3c95a4cfab1b39c38012ff03
wadada <w.daiki1175@gmail.com>
2025-04-27 21:22:20 +0900

    使用しないので

--- src/pages/contact/confirm/index.tsx
+++ src/pages/contact/confirm/index.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import Hero from "@/components/sections/hero/Hero";
-import Confirm from "@/components/sections/contact/Confirm";
-import styles from "@/styles/layout/TopPage.module.scss";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
+// import React from "react";
+// import Hero from "@/components/sections/hero/Hero";
+// import Confirm from "@/components/sections/contact/Confirm";
+// import styles from "@/styles/layout/TopPage.module.scss";
+// import Header from "@/components/layout/Header";
+// import Footer from "@/components/layout/Footer";
 
-const index = () => {
-  return (
-    <>
-      <Header />
-      <div className={styles.container}>
-        <Hero />
-        <Confirm />
-      </div>
-      <Footer />
-    </>
-  );
-};
+// const index = () => {
+//   return (
+//     <>
+//       <Header />
+//       <div className={styles.container}>
+//         <Hero />
+//         <Confirm />
+//       </div>
+//       <Footer />
+//     </>
+//   );
+// };
 
-export default index;
+// export default index;
