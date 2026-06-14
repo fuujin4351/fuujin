aa4cef15e50f9bb52972c5d829c2129ba1f323f8
fuujin4351 <168528169+fuujin4351@users.noreply.github.com>
2026-06-15 05:02:18 +0900

    Update enbuData.ts

--- src/components/sections/enbu/enbuData.ts
+++ src/components/sections/enbu/enbuData.ts
@@ -7,7 +7,7 @@ export const enbuList = [
     description: "風に宿るは雷鳴の轟き\nいま神を迎ふるとき",
     url: "https://www.youtube.com/embed/hr7AcHxZwiY",
     img: "/enbu/2025.webp",
-    startColor: "#4A0F0F", // 開始色
+    startColor: "#B11226", // 開始色
     endColor: "#D4AF37", // 終了色
   },
   {
